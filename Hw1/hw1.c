faf7f4868eca8e57782ce09038936d6f83b4996c
KCGO57 <kjc0765@gmail.com>
2026-07-13 15:49:42 -0700

    .

---
+++ Hw1/hw1.c
@@ -0,0 +1,11 @@
+#include "hw1.h"
+
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+
+double area_of_disk(double radius) {
+    double area = M_PI*(radius*radius);
+    return area;
+}
